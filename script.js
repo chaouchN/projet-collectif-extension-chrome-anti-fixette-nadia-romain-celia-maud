fe3afe530ba1e717092c74eca8de82c9b57d8f8b
cetrl <celiatruel@gmail.com>
2024-04-29 15:52:05 +0200

    pop-up lien random

---
+++ script.js
@@ -0,0 +1,11 @@
+//tableau de liens
+const linkArray = ["lien 0", "lien 1", "lien 2", "lien 3"];
+
+//appelle la fonction popup toute les X millis avec comme paramètre 4
+const intervalID = setInterval(popUp, 1000, 4);
+
+//alerte qui affiche le paramètre randomisé par la multiplication puis transformé en entier par math.floor
+function popUp(a) {
+  alert(linkArray[Math.floor(Math.random() * a)]);
+}
+
