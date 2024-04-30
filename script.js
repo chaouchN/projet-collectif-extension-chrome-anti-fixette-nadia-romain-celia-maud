cba952be1309727298aa7624107b0df9f1b3fc76
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-04-30 11:22:14 +0200

    script.js V2 ok

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //tableau de liens
-const linkArray = ["lien 0", "lien 1", "lien 2", "lien 3"];
+const linkArray = ["https://www.youtube.com/watch?v=4_1sKQP3Uo4", "https://wikiroulette.co/", "https://thesecatsdonotexist.com/", "https://theuselessweb.com/", "https://www.kamoulox.lol/","https://fr.wikihow.com/faire-une-pause-avec-les-r%C3%A9seaux-sociaux","https://fr.wikihow.com/surmonter-la-cyberd%C3%A9pendance", "https://lasonotheque.org/jeux/quiz-sonore", "https://perdu.com/", "http://make-everything-ok.com/", "https://pointerpointer.com/"];
 
 //appelle la fonction popup toute les X millis avec comme paramètre 4
-const intervalID = setInterval(popUp, 1000, 4);
+const intervalID = setInterval(popUp, 1000000, linkArray.length);
 
 //alerte qui affiche le paramètre randomisé par la multiplication puis transformé en entier par math.floor
 function popUp(a) {
-  alert(linkArray[Math.floor(Math.random() * a)]);
+  window.open(linkArray[Math.floor(Math.random() * a)]);
 }
 
