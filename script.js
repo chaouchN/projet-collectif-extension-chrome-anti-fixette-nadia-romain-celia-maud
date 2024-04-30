680b3366ab22292f98be735c34ea0d705c3aefd0
chaouchN <chaouch.nadia@hotmail.com>
2024-04-30 11:35:35 +0200

    jpp

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const linkArray = ["lien 0", "lien 1", "lien 2", "lien 3"];
 
 //appelle la fonction popup toute les X millis avec comme paramètre 4
-const intervalID = setInterval(popUp, 1000, 4);
+const intervalID = setInterval(popUp, 10000, 4);
 
 //alerte qui affiche le paramètre randomisé par la multiplication puis transformé en entier par math.floor
 function popUp(a) {
